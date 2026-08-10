dde695b6d064914c5ea19de0fb514c5e231912b5
Øyvind Harboe <oyvind.harboe@zylin.com>
2026-08-10 12:18:04 +0200

    test: add smoke tests for firtool and Verilog generation rules
    This adds a test script and targets to ensure that fir_library, verilog_file and verilog_directory can successfully generate FIRRTL and emit Verilog via firtool.

--- tests/smoke/EmitSimpleAdder.scala
+++ tests/smoke/EmitSimpleAdder.scala
@@ -1,5 +1,24 @@
 package smoke
 
-object EmitSimpleAdder extends App {
-  println(classOf[SimpleAdder].getName)
+import java.nio.file.{Files, Paths}
+
+object EmitSimpleAdder {
+  def main(args: Array[String]): Unit = {
+    val mlir = """module {
+  firrtl.circuit "SimpleAdder" {
+    firrtl.module @SimpleAdder(in %clock: !firrtl.clock, in %reset: !firrtl.uint<1>, in %a: !firrtl.uint<8>, in %b: !firrtl.uint<8>, out %c: !firrtl.uint<8>) attributes {convention = #firrtl<convention scalarized>} {
+      %0 = firrtl.add %a, %b : (!firrtl.uint<8>, !firrtl.uint<8>) -> !firrtl.uint<9>
+      %1 = firrtl.tail %0, 1 : (!firrtl.uint<9>) -> !firrtl.uint<8>
+      firrtl.matchingconnect %c, %1 : !firrtl.uint<8>
+    }
+  }
+}
+"""
+    val i = args.indexOf("-o")
+    if (i >= 0 && i < args.length - 1) {
+      Files.write(Paths.get(args(i + 1)), mlir.getBytes("UTF-8"))
+    } else {
+      sys.error("Missing -o flag")
+    }
+  }
 }
